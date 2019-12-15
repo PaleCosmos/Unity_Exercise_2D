0fe90cd99e7333141ce31ef0bc6a5c7c8762d33e
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-12-15 18:10:46 +0900

    ㅇㅗ잉

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    public float maxSpeed;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed) // right
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < -maxSpeed) // left
+            rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            anim.SetBool("isWalking", false);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        if(Input.GetButtonDown("Horizontal"))
+        {
+            anim.SetBool("isWalking", true);
+            spriteRenderer.flipX = System.Math.Abs(Input.GetAxisRaw("Horizontal") - -1) < 0.001;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
