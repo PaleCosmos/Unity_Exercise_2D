c569819f673093e0c09db9715a591b6a3fc3e62c
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-12-16 17:11:51 +0900

    jump

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,7 +4,11 @@ public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
-    public float maxSpeed;
+    public float movePower;
+    public float jumpPower;
+
+    private Vector3 lastVelocity;
+
     Animator anim;
 
     // Start is called before the first frame update
@@ -13,35 +17,68 @@ public class PlayerMove : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+
+        lastVelocity = rigid.velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
+        Jump();
+    }
 
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+    private void FixedUpdate()
+    {
+        Move();
+        Ray();
+    }
 
-        if (rigid.velocity.x > maxSpeed) // right
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        else if (rigid.velocity.x < -maxSpeed) // left
-            rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
 
-        if (Input.GetButtonUp("Horizontal"))
+        if(Input.GetAxisRaw("Horizontal") < 0 )
         {
-            anim.SetBool("isWalking", false);
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+            spriteRenderer.flipX = true;
+            moveVelocity = Vector3.left;
+        }else if(Input.GetAxisRaw("Horizontal") > 0 )
+        {
+            spriteRenderer.flipX = false;
+            moveVelocity = Vector3.right;
         }
+        anim.SetBool("isWalking", (System.Math.Abs(Input.GetAxisRaw("Horizontal")) > 0.001));
 
-        if(Input.GetButtonDown("Horizontal"))
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+
+        if(System.Math.Abs(rigid.velocity.y) < 0.01 && lastVelocity.y < 0f)
         {
-            anim.SetBool("isWalking", true);
-            spriteRenderer.flipX = System.Math.Abs(Input.GetAxisRaw("Horizontal") - -1) < 0.001;
+            anim.SetBool("isJumping", false);
+        }
+        lastVelocity = rigid.velocity;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
         }
     }
 
-    void FixedUpdate()
+    void Ray()
     {
-        
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1f);
+
+        if(rayHit.collider != null)
+        {
+            if(rayHit.distance <= 0.5f && rigid.velocity.y < 0f)
+            {
+                anim.SetBool("isJumping", false);
+            }
+            Debug.Log(rayHit.distance);
+        }
     }
 }
